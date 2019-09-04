2c49f0c3465da40940b30ec674038dac9aa08305
Alexandr Tkach <TkachSaha@mail.ru>
2019-09-04 21:30:26 +0300

    Добавленно сохранение настроек

--- content.js
+++ content.js
@@ -1,79 +1,91 @@
-alert(localStorage['hint_checkbox_state']);
-// ============================================================== PARSING ===========================================
 
-console.log("=======");
-var link = document.querySelector('#test-question').getAttribute('action');
-var start_id_index = "https://att.elma-bpm.ru/testing/".length;
-var len_id = link.slice(start_id_index).indexOf('/');
+if (document.querySelector('.records-history > h2') != null) {
 
-var test_id = link.substr(start_id_index, len_id);
-console.log("Id компетенции: " + test_id);
+    console.log("=======");
+    console.log('Вы на странице записи на аттестацию');
+    console.log("=======");
 
-console.log("=======");
+} else if (document.querySelector('.skip-question') != null) {
 
-var subject = document.querySelector(".testing-heading > div > span > b").innerHTML;
+    console.log("=======");
+    console.log('Вы на странице вопроса');
+    console.log("=======");
 
-console.log(subject);
-
-console.log("=======");
-
-try {
-    var questionType = document.querySelectorAll("[name=answer_value]")[0].getAttribute("type");    
-} catch (error) {
-    var questionType = document.querySelectorAll("[name='answer_value[]']")[0].getAttribute("type");    
-}
-
-questionType = questionType == null ? "textarea" : questionType;
-console.log("Тип вопроса: " + questionType);
+    // ============================================================== PARSING ===========================================
 
 
-console.log("=======");
+    var link = document.querySelector('#test-question').getAttribute('action');
+    var start_id_index = "https://att.elma-bpm.ru/testing/".length;
+    var len_id = link.slice(start_id_index).indexOf('/');
 
-var question = document.querySelector(".question > div > p").innerHTML;
+    var test_id = link.substr(start_id_index, len_id);
+    console.log("Id компетенции: " + test_id);
 
+    console.log("=======");
 
+    var subject = document.querySelector(".testing-heading > div > span > b").innerHTML;
 
-console.log("=======");
+    console.log(subject);
 
-console.log("Вопрос: " + question);
+    console.log("=======");
 
-var questionId = document.querySelector("input[name=question_id]").value;
-
-console.log("ID вопроса: " + questionId);
-console.log("=======");
-console.log("=======");
-
-
-
-//======================================================= GENERATING JS OBJECT FOR CONVERTING TO JSON =============================
-
-var object_info = {
-    test: test_id,
-    subject: subject,
-    type: questionType,
-    question_id: questionId,
-    question_text: question,
-    answers: {
-
+    try {
+        var questionType = document.querySelectorAll("[name=answer_value]")[0].getAttribute("type");
+    } catch (error) {
+        var questionType = document.querySelectorAll("[name='answer_value[]']")[0].getAttribute("type");
     }
-}
 
-try {
-    var answersLabels = document.querySelectorAll(".answer-text");
+    questionType = questionType == null ? "textarea" : questionType;
+    console.log("Тип вопроса: " + questionType);
 
-    answersLabels.forEach(function (item, index, arr) {
-        var answer_id = item.getAttribute("for").slice(questionType.length);
-        var answer_text = item.innerHTML.replace(/\s+/g, ' ').trim();;
-        console.log("ID ответа: " + answer_id);
-        console.log(answer_text);
-        console.log("=======");
-        object_info.answers[answer_id] = answer_text;
-    });
-} catch (error) {
-    console.error(error);
-}
 
-// ============================================================== AJAX ===========================================
+    console.log("=======");
+
+    var question = document.querySelector(".question > div > p").innerHTML;
+
+
+
+    console.log("=======");
+
+    console.log("Вопрос: " + question);
+
+    var questionId = document.querySelector("input[name=question_id]").value;
+
+    console.log("ID вопроса: " + questionId);
+    console.log("=======");
+    console.log("=======");
+
+
+
+    //======================================================= GENERATING JS OBJECT FOR CONVERTING TO JSON =============================
+
+    var object_info = {
+        test: test_id,
+        subject: subject,
+        type: questionType,
+        question_id: questionId,
+        question_text: question,
+        answers: {
+
+        }
+    }
+
+    try {
+        var answersLabels = document.querySelectorAll(".answer-text");
+
+        answersLabels.forEach(function (item, index, arr) {
+            var answer_id = item.getAttribute("for").slice(questionType.length);
+            var answer_text = item.innerHTML.replace(/\s+/g, ' ').trim();;
+            console.log("ID ответа: " + answer_id);
+            console.log(answer_text);
+            console.log("=======");
+            object_info.answers[answer_id] = answer_text;
+        });
+    } catch (error) {
+        console.error(error);
+    }
+
+    // ============================================================== AJAX ===========================================
     $.ajax({
         url: 'http://localhost:1111',
         data: object_info,
@@ -81,27 +93,35 @@ try {
         datatype: 'json',
 
         success: function (answer_info) {
-            
+
             //for debug
             alert(answer_info);
 
             answer_info = JSON.parse(answer_info);
 
-            if(answer_info.error !== undefined){
+            if (answer_info.error !== undefined) {
                 console.error(answer_info.error);
-            } else if (!answer_info.already_contains){
+            } else if (!answer_info.already_contains) {
                 console.success("Вопрос добавлен в базу");
-            } else {                
+            } else {
 
-                document.querySelectorAll(".answers > div > input").forEach(function(item, i, arr){
-                    if(answer_info.answer_ids.includes(item.value)){
-                         item.parentNode.style.backgroundColor = "lightgreen";
-                     }
+                //TODO Протестировать
+                chrome.storage.sync.get(['hint_checkbox_state'], function (result) {
+                    var res = result.hint_checkbox_state;
+                    if (res) {
+                        document.querySelectorAll(".answers > div > input").forEach(function (item, i, arr) {
+                            if (answer_info.answer_ids.includes(item.value)) {
+                                item.parentNode.style.backgroundColor = "lightgreen";
+                            }
+                        });
+                    }
                 });
+
+                
 
 
             }
-            
-        }
-    })
 
+        }
+    });
+}
--- popup.js
+++ popup.js
@@ -1,9 +1,14 @@
 
 $('#hint_checkbox').click(function(){
-    console.log(localStorage['hint_checkbox_state']);
-    localStorage['hint_checkbox_state'] = this.checked;
+
+    chrome.storage.sync.set({hint_checkbox_state: this.checked}, function() {});
 });
 
-console.log(localStorage['hint_checkbox_state']);
-
-$('#hint_checkbox').prop('checked', localStorage['hint_checkbox_state'] == 'true');
+chrome.storage.sync.get(['hint_checkbox_state'], function(result) {
+    var res = result.hint_checkbox_state;
+    if(res == undefined){
+        chrome.storage.sync.set({hint_checkbox_state: true}, function() {});
+        res = true;
+    }
+    $('#hint_checkbox').prop('checked', res);
+});
